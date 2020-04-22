5902fa5fa174d7dbc0a467bcbb4129fe22a4dc76
sabyasachi.nandy <sabya.nandy14@gmail.com>
2020-04-22 23:26:14 +0530

    Step 3 done.

--- code/models/order_item.py
+++ code/models/order_item.py
@@ -1,13 +1,17 @@
 class OrderItem:
-    def __init__(self, product, quantity):
+    def __init__(self, product, quantity, user_type=None):
         self._product = product
         self._quantity = quantity
+        self._user_type = user_type
 
     def get_price(self):
         return self._product.get_price() * self._quantity
 
     def get_discounted_price(self):
         if self._quantity > 10:
-            return self.get_price() * 0.9
+            price = self.get_price() * 0.9
         else:
-            return self.get_price()
+            price = self.get_price()
+        if self._user_type == "special":
+            price = price * 0.95
+        return price
--- test/models/test_order_item.py
+++ test/models/test_order_item.py
@@ -21,3 +21,10 @@ class TestOrderItem(TestCase):
 
         item_11_quantity = OrderItem(product_mock, 11)
         assert item_11_quantity.get_discounted_price() == 99
+
+    @patch("code.models.product.Product")
+    def test_a_order_item_should_gives_extra_10_percent_when_user_type_special(self, product_mock):
+        product_mock.get_name.return_value = 'test-val-1'
+        product_mock.get_price.return_value = 10
+        item_20_quantity = OrderItem(product_mock, 20, "special")
+        assert item_20_quantity.get_discounted_price() == 171
