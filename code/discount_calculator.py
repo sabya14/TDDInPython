88cbe3d610f9100613bbb139cc6919198df25403
sabyasachi.nandy <sabya.nandy14@gmail.com>
2020-04-22 21:02:51 +0530

    2nd step done

--- code/discount_calculator.py
+++ code/discount_calculator.py
@@ -1,18 +1,21 @@
 def calculate_discount(purchases_count):
-    if purchases_count > 10:
+    if purchases_count > 30:
+        return 30
+    elif purchases_count > 20:
+        return 20
+    elif purchases_count > 10:
         return 10
     else:
         return 0
 
 
-def get_product_price(product_price):
-    previous_purchase = 50
+def get_product_price(product_price, previous_purchase):
     discount = calculate_discount(previous_purchase)
-    return (100 - discount)/100 * product_price
+    return (100 - discount) /100 * product_price
 
 
 def driver():
-    price = get_product_price(100)
+    price = get_product_price(100, previous_purchase=50)
     print(price)
 
 
--- readme.md
+++ readme.md
@@ -2,3 +2,10 @@
 Step 1:
 Given a user and the number of times he/she has bought my product give proper discount
 If More than 10 products, 10 percent discount.
+
+Step 2:
+If more that 20 products , 20 percent
+If more that 30 products , 30 percent
+
+Step 3:
+If user is type special, give additional 5 percent.
